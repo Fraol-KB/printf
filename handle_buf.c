f8606395ce2ded9fe2d0d541ae25e971a0ec6f1e
Fraol Kelbessa Beyene <fraolkelbessa@gmail.com>
2022-08-09 17:12:01 +0300

    hot -fix

--- handle_buf.c
+++ handle_buf.c
@@ -1,26 +1,20 @@
 #include "main.h"
 
 /**
- * print_str - writes the string to stdout
- * @arguments: input string
+ * handle_buf - concatenates the buffer characters
  * @buf: buffer pointer
- * @ibuf: index for buffer pointer
- * Return: On success 1.
+ * @c: charcter to concatenate
+ * @ibuf: index of buffer pointer
+ * Return: index of buffer pointer.
  */
-int print_str(va_list arguments, char *buf, unsigned int ibuf)
+unsigned int handle_buf(char *buf, char c, unsigned int ibuf)
 {
-	char *str;
-	unsigned int i;
-	char nill[] = "(null)";
-
-	str = va_arg(arguments, char *);
-	if (str == NULL)
+	if (ibuf == 1024)
 	{
-		for (i = 0; nill[i]; i++)
-			ibuf = handl_buf(buf, nill[i], ibuf);
-		return (6);
+		print_buf(buf, ibuf);
+		ibuf = 0;
 	}
-	for (i = 0; str[i]; i++)
-		ibuf = handl_buf(buf, str[i], ibuf);
-	return (i);
+	buf[ibuf] = c;
+	ibuf++;
+	return (ibuf);
 }
