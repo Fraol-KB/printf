38481cb0f62ed5390ed0020760f70a8bfcb3db32
Fraol Kelbessa Beyene <fraolkelbessa@gmail.com>
2022-08-09 16:39:44 +0300

    error fixes

--- _printf.c
+++ _printf.c
@@ -13,33 +13,38 @@ int _printf(const char *format, ...)
 	int (*function)(va_list, char *, unsigned int);
 	char *buffer;
 
-	va_start(arguments, format);
-	buffer = malloc(sizeof(char) * 1024);
-	while (format && format[i])
+	va_start(arguments, format), buffer = malloc(sizeof(char) * 1024);
+	if (!format || !buffer || (format[i] == '%' && !format[i + 1]))
+		return (-1);
+	if (!format[i])
+		return (0);
+	for (i = 0; format && format[i]; i++)
 	{
 		if (format[i] == '%')
 		{
 			if (format[i + 1] == '%')
-				handl_buf(buffer, format[i], ibuf), len++;
+				handle_buf(buffer, format[i], ibuf), len++;
 			else if (format[i + 1] == '\0')
 			{
-				if (ibuf != 0)
-					print_buf(buffer, ibuf);
-				free(buffer);
+				print_buf(buffer, ibuf), free(buffer), va_end(arguments);
 				return (-1);
 			}
 			else
 			{
-				function = get_print_func(format, i + 1);
-				len += function(arguments, buffer, ibuf);
-			}
-			i++;
+				function = get_print_func(format[i + 1]);
+				if (function == NULL)
+				{
+					handle_buf(buffer, format[i], ibuf);
+					len++, i--;
+				}
+				else
+					len += function(arguments, buffer, ibuf);
+			} i++;
 		}
 		else
-			handl_buf(buffer, format[i], ibuf), len++;
-		i++, ibuf = len;
-		while (ibuf > 1024)
-			ibuf -= 1024;
+			handle_buf(buffer, format[i], ibuf), len++;
+		for (ibuf = len; ibuf > 1024; ibuf -= 1024)
+			;
 	}
 	print_buf(buffer, ibuf), free(buffer), va_end(arguments);
 	return (len);
